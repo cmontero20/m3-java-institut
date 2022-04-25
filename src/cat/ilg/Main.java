dfad15aa6007a0d71492e8eb54f5c8887c20b466
César <cmontero20@ilg.cat>
2022-04-25 17:38:19 +0200

    Commit 25/4/2022

--- src/cat/ilg/Main.java
+++ src/cat/ilg/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Persona p = new Persona();
+        // p.nom = "César";
+        // p.dni = "666X"
+        p.canviarNom("César");
+        p.asignarDni("6666");
+        System.out.println(p.getNom());
+        System.out.println(p.getDni());
+        System.out.println(p.obtenirDades());
     }
 }
